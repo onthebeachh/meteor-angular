1310a6820b2b73fa773896595695fcbc9f454f2a
Sergio Esteban <sergio.cardenas@radeonlabs.com>
2015-12-12 10:02:06 -0300

    7. Running on mobile

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -3,11 +3,20 @@ Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
 
   angular.module('simple-todos',['angular-meteor']);
+
+  function onReady() {
+    angular.bootstrap(document, ['simple-todos']);
+  }
+ 
+  if (Meteor.isCordova)
+    angular.element(document).on('deviceready', onReady);
+  else
+    angular.element(document).ready(onReady);
+  
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
  
       $scope.tasks = $meteor.collection(function () {
-
         return Tasks.find({}, {sort: {createdAt: -1}})
       });
 
