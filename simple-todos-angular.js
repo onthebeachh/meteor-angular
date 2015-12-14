f6d373201f511eb855ac366519705a39eebddfab
Sergio Esteban <sergio.cardenas@radeonlabs.com>
2015-12-14 02:25:48 -0300

    Security with methods

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -25,12 +25,15 @@ if (Meteor.isClient) {
       });
 
       $scope.addTask = function(newTask) {
-        $scope.tasks.push( {
-            text: newTask,
-            createdAt: new Date(),             // current time
-            owner: Meteor.userId(),            // _id of logged in user
-            username: Meteor.user().username }  // username of logged in user
-        );
+        $meteor.call('addTask',newTask)
+      };
+
+      $scope.deleteTask = function (task) {
+        $meteor.call('deleteTask', task._id);
+      };
+ 
+      $scope.setChecked = function (task) {
+        $meteor.call('setChecked', task._id, !task.checked);
       };
 
       $scope.$watch('hideCompleted', function() {
@@ -45,4 +48,26 @@ if (Meteor.isClient) {
       };
  
     }]);
+
+  Meteor.methods({
+    addTask: function (text) {
+      // Make sure the user is logged in before inserting a task
+      if (! Meteor.userId()) {
+        throw new Meteor.Error('not-authorized');
+      }
+   
+      Tasks.insert({
+        text: text,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username
+      });
+    },
+    deleteTask: function (taskId) {
+      Tasks.remove(taskId);
+    },
+    setChecked: function (taskId, setChecked) {
+      Tasks.update(taskId, { $set: { checked: setChecked} });
+    }
+  });
 }
